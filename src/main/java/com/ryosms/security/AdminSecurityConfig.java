ef78ad9967c4a23344c72b9f02bce16d6bb48094
ryosms <ryo.sms@gmail.com>
2016-12-24 13:43:35 +0900

    Add Spring security config class for admin login

---
+++ src/main/java/com/ryosms/security/AdminSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.ryosms.security;
+
+import com.ryosms.security.admin.domain.service.AdminLoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Created by ryosms on 2016/12/24.
+ */
+@Configuration
+@EnableWebSecurity
+@Order(1)
+public class AdminSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    AdminLoginService adminLoginService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin/login").permitAll()
+                .antMatchers("/admin/login/failure").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN");
+        http.formLogin()
+                .loginProcessingUrl("/admin/login")
+                .failureForwardUrl("/admin/login/failure")
+                .loginPage("/admin/login")
+                .defaultSuccessUrl("/admin/menu")
+                .usernameParameter("login_id")
+                .passwordParameter("password");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(adminLoginService)
+                .passwordEncoder(passwordEncoder());
+    }
+}
--- src/main/java/com/ryosms/security/admin/controller/AdminController.java
+++ src/main/java/com/ryosms/security/admin/controller/AdminController.java
@@ -1,7 +1,9 @@
 package com.ryosms.security.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by ryosms on 2016/12/21.
@@ -11,6 +13,13 @@ public class AdminController {
 
     @RequestMapping("/admin/login")
     public String adminLogin() {
+        return "admin/login";
+    }
+
+    @RequestMapping("/admin/login/failure")
+    public String adminLoginFailure(@RequestParam("login_id") String loginId,
+                                    Model model) {
+        model.addAttribute("login_id", loginId);
         return "admin/login";
     }
 
