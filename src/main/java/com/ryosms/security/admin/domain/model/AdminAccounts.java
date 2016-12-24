c40d5f057fdf5d2c6e30f06c8430006c712fef7d
ryosms <ryo.sms@gmail.com>
2016-12-24 13:22:06 +0900

    Mod model class

--- src/main/java/com/ryosms/security/admin/domain/model/AdminAccounts.java
+++ src/main/java/com/ryosms/security/admin/domain/model/AdminAccounts.java
@@ -4,11 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by ryosms on 2016/12/24.
@@ -19,7 +25,7 @@ import javax.persistence.Table;
 @Builder
 @Entity
 @Table(name = "admin_accounts", catalog = "spring_security")
-public class AdminAccounts {
+public class AdminAccounts implements UserDetails {
     @Id
     @Column(name = "login_id")
     String loginId;
@@ -29,4 +35,36 @@ public class AdminAccounts {
 
     @Column(name = "admin_name")
     String adminName;
+
+    private static final List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_ADMIN"));
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return loginId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
