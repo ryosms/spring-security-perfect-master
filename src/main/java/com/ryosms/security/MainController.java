2ebdbc2c1e4ba5cac6e0f67bebfad411c6a6931e
ryosms <ryo.sms@gmail.com>
2016-12-24 13:59:04 +0900

    Add Main menu

---
+++ src/main/java/com/ryosms/security/MainController.java
@@ -0,0 +1,17 @@
+package com.ryosms.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by ryosms on 2016/12/24.
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String mainMenu() {
+        return "menu";
+    }
+
+}
