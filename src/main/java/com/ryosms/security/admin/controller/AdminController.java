32a7c6e580bfcbace5cfd80e9d02e364c80e59c8
ryosms <ryo.sms@gmail.com>
2016-12-24 12:53:32 +0900

    Mod packages

--- src/main/java/com/ryosms/security/admin/controller/AdminController.java
+++ src/main/java/com/ryosms/security/admin/controller/AdminController.java
@@ -1,4 +1,4 @@
-package com.ryosms.security.controller;
+package com.ryosms.security.admin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
