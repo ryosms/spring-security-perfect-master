e7a208e58b5465ecc8f420a56c3312ed044be69e
ryosms <ryo.sms@gmail.com>
2016-12-26 22:56:47 +0900

    Add spring session

--- src/main/java/com/ryosms/security/MainClass.java
+++ src/main/java/com/ryosms/security/MainClass.java
@@ -2,11 +2,13 @@ package com.ryosms.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * Created by ryosms on 2016/12/21.
  */
 @SpringBootApplication
+@EnableRedisHttpSession
 public class MainClass {
     public static void main(String[] args) {
         SpringApplication.run(MainClass.class, args);
